ae1bf7e85aad7efb60bbb9823a33434a53833e8a
EthanChill <115500674+EthanChill@users.noreply.github.com>
2023-04-13 12:18:05 +0300

    Update connection.php

--- PHP/connection.php
+++ PHP/connection.php
@@ -3,11 +3,9 @@ $host="localhost";
 $user="id9520144_root";
 $password = "root";
 $db_name="id9520144";
-$connect = mysql_connect($host,$user,$password,$db_name);
+$connect = mysqli_connect($host,$user,$password,$db_name);
 if(!$connect){
-	die('error'.mysqli.error());
-	
-	
+	die('error'.mysqli.error());
 }
 // Обновление таблицы заказов
 if(isset($_POST['addr'])){
@@ -18,8 +16,7 @@ if(isset($_POST['addr'])){
 	$id = $_POST['id'];
 	$query="Update ordertab set addr='".$addr."',telm='".$telm."',fioz='".$fioz."',price='".$price."' where id ='".$id."'";
 	$result = mysqli_query($connect,query);
-	if($result){
-		
+	if($result){
 		echo 'data updated';
 	}
 }
