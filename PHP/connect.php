6041eea848ceccffab460562fe46e9b457f97292
EthanChill <115500674+EthanChill@users.noreply.github.com>
2023-04-13 12:17:24 +0300

    Update connect.php

--- PHP/connect.php
+++ PHP/connect.php
@@ -4,7 +4,7 @@ $host="localhost";
 $user="id9520144_root";
 $password = "root";
 $db_name="id9520144";
-$connect = mysql_connect($host,$user,$password,$db_name);
+$connect = mysqli_connect($host,$user,$password,$db_name);
 if(!$connect){
 	die('error'.mysqli.error());
 	
